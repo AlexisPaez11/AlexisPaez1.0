edafda4f7cb873600c31b290067ac441564d006a
Alexis Paez Carrazan <77308122+AlexisPaez11@users.noreply.github.com>
2022-06-14 20:18:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+// MENU
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_disabled");
 });
+
+// BUTTON UP
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 2));
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function() {
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 400) {
+        buttonUp.style.transform = "scale(1)";
+    }
+    else if (scroll < 400) {
+        buttonUp.style.transform = "scale(0)";
+    }
+}
