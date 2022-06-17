e84ddb8e220566239a255766df90aa7b6d5b3223
Alexis Paez Carrazan <77308122+AlexisPaez11@users.noreply.github.com>
2022-06-16 21:14:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ document.getElementById("button-up").addEventListener("click", scrollUp);
 function scrollUp() {
     var currentScroll = document.documentElement.scrollTop;
     if (currentScroll > 0) {
-        window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 2));
+        window.scrollTo (0, 0);
         buttonUp.style.transform = "scale(0)";
     }
 }
