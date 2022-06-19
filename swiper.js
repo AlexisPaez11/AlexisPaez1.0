c1aed2e022f7ee08bccf9361b1c0c77083096e28
Alexis Paez Carrazan <77308122+AlexisPaez11@users.noreply.github.com>
2022-06-18 23:18:29 -0300

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,16 @@
+// SWIPER
+var swiper = new Swiper(".mySwiperProjects", {
+    slidesPerView: "auto",
+    navigation: {
+        nextEl: ".swiper-button-next-projects",
+        prevEl: ".swiper-button-prev-projects",
+    },
+});
+
+var swiper = new Swiper(".mySwiperBlog", {
+    slidesPerView: "auto",
+    navigation: {
+        nextEl: ".swiper-button-next-blog",
+        prevEl: ".swiper-button-prev-blog",
+    },
+});
